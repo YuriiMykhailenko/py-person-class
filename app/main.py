84c65b14fffa8a07b5200c60663d2eaf98563fbb
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2025-12-29 14:48:03 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -12,9 +12,11 @@ def create_person_list(people: list) -> list:
     instances = [Person(p["name"], p["age"]) for p in people]
     for data, inst in zip(people, instances):
         spouse_name = data.get("wife")
-        if spouse_name is not None and Person.people.get(spouse_name) is not None:
-            inst.wife = Person.people.get(spouse_name)
+        spouse = Person.people.get(spouse_name) if spouse_name else None
+        if spouse:
+            inst.wife = spouse
         spouse_name = data.get("husband")
-        if spouse_name is not None and Person.people.get(spouse_name) is not None:
-            inst.husband = Person.people.get(spouse_name)
+        spouse = Person.people.get(spouse_name) if spouse_name else None
+        if spouse:
+            inst.husband = spouse
     return instances
