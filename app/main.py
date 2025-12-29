e7959319a8d6a0ee247795acf0641d02ec483aa7
YuriiMykhailenko <urijmihajlenko3@gmail.com>
2025-12-29 14:43:37 +0200

    Implement Person class and create_person_list function

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    Person.people.clear()
+    instances = [Person(p["name"], p["age"]) for p in people]
+    for data, inst in zip(people, instances):
+        spouse_name = data.get("wife")
+        if spouse_name is not None:
+            spouse = Person.people.get(spouse_name)
+            if spouse is not None:
+                inst.wife = spouse
+        spouse_name = data.get("husband")
+        if spouse_name is not None:
+            spouse = Person.people.get(spouse_name)
+            if spouse is not None:
+                inst.husband = spouse
+    return instances
